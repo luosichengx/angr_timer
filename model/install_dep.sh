dee4dca593edbead7b5edffb8125e6abad18f9e5
jjf <jjf@jjfdeMacBookPro.local>
2020-06-30 15:44:30 +0800

    add pytorch-cpu in docker

--- model/install_dep.sh
+++ model/install_dep.sh
@@ -3,3 +3,4 @@ source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 workon angr
 #add new dependency here
 pip install dgl
+pip install torch==1.5.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
